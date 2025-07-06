5fc93cb2c1bf3e16d49a0ff4c8a03f4998bdb203
Oliver Ilp <oliver@oliverilp.ee>
2025-07-06 22:17:37 +0300

    Increase memory for Kubernetes master nodes

--- terraform/k8s-cluster.tf
+++ terraform/k8s-cluster.tf
@@ -5,7 +5,7 @@ module "k8s_master" {
   vmid_prefix = "40"
   name_prefix = "k8s-master"
   vm_state    = "running"
-  memory      = 4096
+  memory      = 5120
   disk_size   = 20
   ip_base     = "10.1.1.3"
   ip_offset   = 1
